cdad2226bcc2244bcbf52111d264c80fe43e83c1
LahenAnjula <lahen.20212098@iit.ac.lk>
2024-02-16 10:40:46 +0530

    Change the folder structure of the PSI-13

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,17 +1,7 @@
-import { useState } from "react";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div className="App">
-      <Header />
-      <Footer />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;

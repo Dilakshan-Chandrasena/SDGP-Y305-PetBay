46aaeb533e6d619765e51e12738474d4c0ada36c
LahenAnjula <lahen.20212098@iit.ac.lk>
2024-02-16 10:06:30 +0530

    Update questions

--- Client/src/assets/questionsData.js
+++ Client/src/assets/questionsData.js
@@ -8,20 +8,27 @@ export const questionsData = [
     option5: "Extremely tough",
   },
   {
-    question: "What size dog would you like?",
-    option1: "Extra small",
-    option2: "Small",
-    option3: "Medium",
-    option4: "Large",
-    option5: "Extra large",
+    question:
+      "How important is it for your ideal dog breed to adapt well to apartment living?",
+    option1:
+      "Not important - I prefer a breed that doesn't necessarily need to adapt to apartment living",
+    option2:
+      "Somewhat important - I'd prefer a breed that can manage in an apartment but it's not a top priority.",
+    option3:
+      "Moderately important - I need a breed that can comfortably live in an apartment environment.",
+    option4:
+      "Very important - It's crucial that my chosen breed is well-suited for apartment living.",
+    option5:
+      "Extremely important - I only want a breed that thrives in an apartment setting and doesn't require much space.",
   },
   {
-    question: "How friendly is your dog?",
-    option1: "Extremely friendly",
-    option2: "Friendly when its been pet",
-    option3: "Friendly, only with family",
-    option4: "Tough",
-    option5: "Extremely tough",
+    question:
+      "How important is it for your ideal dog breed to adapt well to apartment living?",
+    option1: "Not important",
+    option2: "Somewhat important",
+    option3: "Moderately important",
+    option4: "Very important",
+    option5: "Extremely important",
   },
   {
     question: "What size dog would you like?",

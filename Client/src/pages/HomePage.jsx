4ec508e427cb26f4c0a9ec7372cb5b628650df39
Dilakshan-Chandrasena <dilakshan.20212036@iit.ac.lk>
2024-03-09 21:48:03 +0530

    done deployement

--- Client/src/pages/HomePage.jsx
+++ Client/src/pages/HomePage.jsx
@@ -1,14 +1,16 @@
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import { useLocation } from "react-router-dom";
+import Image from "react-bootstrap/Image";
+import img from "../assets/images/Untitleddesign.png";
+import styles from "../assets/css/homepage.module.css"
 
 export default function HomePage() {
   const userId = window.localStorage.getItem("userId");
 
   return (
     <div>
-      <Header userId={userId}/>
-
+      <Header userId={userId} />
       <Footer />
     </div>
   );
